e13eefa2a98cc83ece5b5c840293361e4315a098
Vitaliy <hykhyk666@gmail.com>
2024-07-15 16:46:29 +0200

    fix: cipher patch

--- build.sh
+++ build.sh
@@ -20,6 +20,20 @@ fi
 # Install the required packages
 pip install -r requirements.txt
 
+# Apply patch to pytube
+patch $(python -c "import pytube; import os; print(os.path.join(os.path.dirname(pytube.__file__), 'cipher.py'))") << 'EOF'
+--- /usr/lib/python3.12/site-packages/pytube/cipher.py	2024-04-05 16:22:49.000000000 -0500
++++ ./cipher.py	2024-07-09 12:34:40.705106637 -0500
+@@ -271,6 +271,7 @@
+         # In the above case, `iha` is the relevant function name
+         r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
+         r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
++        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
+     ]
+     logger.debug('Finding throttling function name')
+     for pattern in function_patterns:
+EOF
+
 # Create the database and tables
 psql -U postgres -f schema.sql
 
